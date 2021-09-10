ec91431ec5639153bc225d168373080abca41197
Nat-Serrano <naticio@gmail.com>
2021-09-10 12:11:44 -0500

    fixed defect fetching from firebase

--- Secret0/HomeView/MatchView.swift
+++ Secret0/HomeView/MatchView.swift
@@ -17,96 +17,107 @@ struct MatchView: View {
     var body: some View {
         
         if model.usersLoaded == nil {
-            LoadingView()
+            ProgressView()
         } else {
-            VStack {
-                //top bar
-                HStack {
-                    Text(model.matches[index].name)
-                        .frame(alignment: .leading)
-                        .font(.title)
-                    
-                    Button(action: {
-                        //action to block or report user
-                    }, label: {
-                        Image(systemName: "ellipsis")
-                    })
-                }
-                
-                //scroll MAIN view
-                ScrollView {
-                    
-                    //first block
-                    VStack {
-                        Group {
-                            Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            Text(model.matches[index].Q1day2live ?? "")
-                        }.padding()
+            
+            if model.matches.count == 0 {
+                Text("No available matches. matches.count == 0")
+            } else {
+                VStack {
+                    //top bar
+                    HStack {
+                        Text(model.matches[index].name)
+                            .frame(alignment: .leading)
+                            .font(.title)
                         
-                        Group {
-                            Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            Text(model.matches[index].QlotteryWin ?? "")
-                        }.padding()
-                        
-                        
-                        Group {
-                            VStack {
-                                HStack {
-                                    Group {
-                                        Image(systemName: "sun.min")
-                                        Text("Age")
-                                    }.padding(.trailing)
-                                    
-                                    Group {
-                                        Image(systemName: "ruler")
-                                        Text(String(model.matches[index].height) ?? "")
-                                    }.padding(.trailing)
-                                    
-                                    Group {
-                                        Image(systemName: "mappin.and.ellipse")
-                                        Text("Location")
-                                    }.padding(.trailing)
-                                }
-                                
-                                Text(model.matches[index].QmoneynotanIssue)
-                                
-                            }
-                        }
-                        
-                        Group {
-                            Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            Text(model.matches[index].bucketList)
-                        }.padding()
-                        
-                        Group {
-                            Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            Text(model.matches[index].jokes)
-                        }.padding()
-                        
-                        Button {
-                            self.index += 1
-                        } label: {
-                            Text("Next")
-                        }.padding(.bottom)
-
-                        //DISLIKE OR NEXT
-//                        NavigationLink(destination: MatchView(index: index + 1), isActive: $goWhenTrue) {
-//
-//                            Button {
-//                                goWhenTrue = true
-//                            } label: {
-//                                Text("Next")
-//                            }
-//                            .padding()
-//                            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.5))
-//                            .frame(width: 100)
-//                        }
+                        Button(action: {
+                            //action to block or report user
+                        }, label: {
+                            Image(systemName: "ellipsis")
+                        })
                     }
                     
-                    
-                    
-                }//scroll view
+                    //scroll MAIN view
+                    ScrollView {
+                        
+                        //first block
+                        VStack {
+                            Group {
+                                Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                Text(model.matches[index].Q1day2live ?? "")
+                            }.padding()
+                            
+                            Group {
+                                Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                Text(model.matches[index].QlotteryWin ?? "")
+                            }.padding()
+                            
+                            
+                            Group {
+                                VStack {
+                                    HStack {
+                                        Group {
+                                            Image(systemName: "sun.min")
+                                            Text("Age")
+                                        }.padding(.trailing)
+                                        
+                                        Group {
+                                            Image(systemName: "ruler")
+                                            Text(String(model.matches[index].height) ?? "")
+                                        }.padding(.trailing)
+                                        
+                                        Group {
+                                            Image(systemName: "mappin.and.ellipse")
+                                            Text("Location")
+                                        }.padding(.trailing)
+                                    }
+                                    
+                                    Text(model.matches[index].QmoneynotanIssue)
+                                    
+                                }
+                            }
+                            
+                            Group {
+                                Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                Text(model.matches[index].bucketList)
+                            }.padding()
+                            
+                            Group {
+                                Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                Text(model.matches[index].jokes)
+                            }.padding()
+                            
+                            Button {
+                                if self.index == model.matches.count-1 {
+                                    //go back to first match
+                                    self.index = 0
+                                } else {
+                                    self.index += 1
+                                }
+                            } label: {
+                                Text("Next")
+                            }.padding(.bottom)
+
+                            //DISLIKE OR NEXT
+    //                        NavigationLink(destination: MatchView(index: index + 1), isActive: $goWhenTrue) {
+    //
+    //                            Button {
+    //                                goWhenTrue = true
+    //                            } label: {
+    //                                Text("Next")
+    //                            }
+    //                            .padding()
+    //                            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.5))
+    //                            .frame(width: 100)
+    //                        }
+                        }
+                        
+                        
+                        
+                    }//scroll view
+                }
             }
+            
 
         
         
