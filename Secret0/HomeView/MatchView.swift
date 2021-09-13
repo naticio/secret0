41f9a22da01a739be596d8507e8dcce30bed1142
Nat-Serrano <naticio@gmail.com>
2021-09-13 00:06:35 -0500

    added reject floating button

--- Secret0/HomeView/MatchView.swift
+++ Secret0/HomeView/MatchView.swift
@@ -10,9 +10,15 @@ import SwiftUI
 struct MatchView: View {
     
     @EnvironmentObject var model: ContentModel
+    
+    @State private var isPresented = false
+    
     @State var index : Int
     @State var image1: String = ""
     @State var goWhenTrue: Bool = false
+    
+    //@State private var scrollViewID = UUID()
+    @State private var reader: ScrollViewProxy?
     
     var body: some View {
         
@@ -37,143 +43,184 @@ struct MatchView: View {
                         Button(action: {
                             //action to block or report user
                         }, label: {
-                            Image(systemName: "ellx ipsis")
+                            Image(systemName: "ellxipsis")
                         })
                     }
                     
                     //scroll MAIN view
-                    ScrollView {
-                        
-                        //first block
-                        VStack {
-                            Group {
-                                
-                                //IOS15 AsyncImage(url: URL(string: "https://your_image_url_address"))
-                                if model.matches[index].imageUrl1 == "" {
-                                    Image(systemName: "person")
-                                        .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                        .aspectRatio(contentMode: .fit)
-                                } else {
+                    
+                    ScrollViewReader {ProxyReader in
+                        ScrollView(.vertical, showsIndicators: false, content: {
+                            
+                            //first block
+                            VStack {
+                                Group {
                                     
-                                    RemoteImage(url: model.matches[index].imageUrl1!)
-                                                .aspectRatio(contentMode: .fit)
-                                                .frame(width: 200)
-                                }
-                                
-//                                Image(uiImage: uiImage ?? UIImage())
-//                                    .resizable()
-//                                    .scaledToFill()
-//                                    .frame(width: 300, height: 300, alignment: .center)
-//                                    .clipped()
-                                
-                                //Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                Text("What would you do if you only have 1 day left to live?")
-                                Text(model.matches[index].Q1day2live ?? "")
-                            }
-//                            .onAppear() {
-//                                model.images = [UIImage()] //make it an empty array first
-//                                model.loadImage(for: model.matches[index].imageUrl1 ?? "")
-//                            }
-                            .padding()
-                            
-                            Group {
-                                //CustomImageView(urlString: model.matches[index].imageUrl2 ?? "")
-                                if model.matches[index].imageUrl2 == "" {
-                                    Image(systemName: "person")
-                                        .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                        .aspectRatio(contentMode: .fit)
-                                } else {
-                                    
-                                    RemoteImage(url: model.matches[index].imageUrl2!)
-                                                .aspectRatio(contentMode: .fit)
-                                                .frame(width: 200)
-                                }
-                                Text(model.matches[index].QlotteryWin ?? "")
-                            }.padding()
-                            
-                            
-                            Group {
-                                VStack {
-                                    HStack {
-                                        Group {
-                                            Image(systemName: "sun.min")
-                                            Text("Age")
-                                        }.padding(.trailing)
+                                    //IOS15 AsyncImage(url: URL(string: "https://your_image_url_address"))
+                                    if model.matches[index].imageUrl1 == "" {
+                                        Image(systemName: "person")
+                                            .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            .aspectRatio(contentMode: .fit)
+                                    } else {
                                         
-                                        Group {
-                                            Image(systemName: "ruler")
-                                            Text(String(model.matches[index].height) ?? "")
-                                        }.padding(.trailing)
-                                        
-                                        Group {
-                                            Image(systemName: "mappin.and.ellipse")
-                                            Text("Location")
-                                        }.padding(.trailing)
+                                        RemoteImage(url: model.matches[index].imageUrl1!)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(width: 200)
                                     }
                                     
-                                    Text(model.matches[index].QmoneynotanIssue)
+    //                                Image(uiImage: uiImage ?? UIImage())
+    //                                    .resizable()
+    //                                    .scaledToFill()
+    //                                    .frame(width: 300, height: 300, alignment: .center)
+    //                                    .clipped()
                                     
+                                    //Image(systemName: "person").frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    Text("What would you do if you only have 1 day left to live?")
+                                    Text(model.matches[index].Q1day2live ?? "")
                                 }
+    //                            .onAppear() {
+    //                                model.images = [UIImage()] //make it an empty array first
+    //                                model.loadImage(for: model.matches[index].imageUrl1 ?? "")
+    //                            }
+                                .padding()
+                                
+                                Group {
+                                    //CustomImageView(urlString: model.matches[index].imageUrl2 ?? "")
+                                    if model.matches[index].imageUrl2 == "" {
+                                        Image(systemName: "person")
+                                            .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            .aspectRatio(contentMode: .fit)
+                                    } else {
+                                        
+                                        RemoteImage(url: model.matches[index].imageUrl2!)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(width: 200)
+                                    }
+                                    Text(model.matches[index].QlotteryWin ?? "")
+                                }.padding()
+                                
+                                
+                                Group {
+                                    VStack {
+                                        HStack {
+                                            Group {
+                                                Image(systemName: "sun.min")
+                                                Text("Age")
+                                            }.padding(.trailing)
+                                            
+                                            Group {
+                                                Image(systemName: "ruler")
+                                                Text(String(model.matches[index].height) ?? "")
+                                            }.padding(.trailing)
+                                            
+                                            Group {
+                                                Image(systemName: "mappin.and.ellipse")
+                                                Text("Location")
+                                            }.padding(.trailing)
+                                        }
+                                        
+                                        Text(model.matches[index].QmoneynotanIssue)
+                                        
+                                    }
+                                }
+                                
+                                Group {
+                                    //CustomImageView(urlString: model.matches[index].imageUrl3 ?? "")
+                                    if model.matches[index].imageUrl3 == "" {
+                                        Image(systemName: "person")
+                                            .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            .aspectRatio(contentMode: .fit)
+                                    } else {
+                                        
+                                        RemoteImage(url: model.matches[index].imageUrl3!)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(width: 200)
+                                    }
+                                    Text(model.matches[index].bucketList)
+                                }.padding()
+                                
+                                Group {
+    //                                CustomImageView(urlString: model.matches[index].imageUrl4 ?? "")
+                                    if model.matches[index].imageUrl4 == "" {
+                                        Image(systemName: "person")
+                                            .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            .aspectRatio(contentMode: .fit)
+                                    } else {
+                                        
+                                        RemoteImage(url: model.matches[index].imageUrl4!)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(width: 200)
+                                    }
+                                    Text(model.matches[index].jokes)
+                                }.padding()
+                            
+
+                                //DISLIKE OR NEXT
+        //                        NavigationLink(destination: MatchView(index: index + 1), isActive: $goWhenTrue) {
+        //
+        //                            Button {
+        //                                goWhenTrue = true
+        //                            } label: {
+        //                                Text("Next")
+        //                            }
+        //                            .padding()
+        //                            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.5))
+        //                            .frame(width: 100)
+        //                        }
                             }
+                            .id("SCROLL_TO_TOP")
                             
-                            Group {
-                                //CustomImageView(urlString: model.matches[index].imageUrl3 ?? "")
-                                if model.matches[index].imageUrl3 == "" {
-                                    Image(systemName: "person")
-                                        .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                        .aspectRatio(contentMode: .fit)
-                                } else {
-                                    
-                                    RemoteImage(url: model.matches[index].imageUrl3!)
-                                                .aspectRatio(contentMode: .fit)
-                                                .frame(width: 200)
-                                }
-                                Text(model.matches[index].bucketList)
-                            }.padding()
                             
-                            Group {
-//                                CustomImageView(urlString: model.matches[index].imageUrl4 ?? "")
-                                if model.matches[index].imageUrl4 == "" {
-                                    Image(systemName: "person")
-                                        .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                        .aspectRatio(contentMode: .fit)
-                                } else {
-                                    
-                                    RemoteImage(url: model.matches[index].imageUrl4!)
-                                                .aspectRatio(contentMode: .fit)
-                                                .frame(width: 200)
-                                }
-                                Text(model.matches[index].jokes)
-                            }.padding()
                             
-                            Button {
+                        })//scroll view
+                        //.id(self.scrollViewID)
+                        //this is to show the rejection button
+                        .overlay(
+                            Button(action: {
+                                //move to the next match
+                                self.isPresented.toggle()
+                                
                                 if self.index == model.matches.count-1 {
                                     //go back to first match
                                     self.index = 0
                                 } else {
                                     self.index += 1
                                 }
-                            } label: {
-                                Text("Next")
-                            }.padding(.bottom)
-
-                            //DISLIKE OR NEXT
-    //                        NavigationLink(destination: MatchView(index: index + 1), isActive: $goWhenTrue) {
-    //
-    //                            Button {
-    //                                goWhenTrue = true
-    //                            } label: {
-    //                                Text("Next")
-    //                            }
-    //                            .padding()
-    //                            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.5))
-    //                            .frame(width: 100)
-    //                        }
-                        }
-                        
-                        
-                        
-                    }//scroll view
+                                
+                                //scroll to top
+                                withAnimation(.spring()) {
+                                    ProxyReader.scrollTo("SCROLL_TO_TOP", anchor: .top)
+                                    
+                                    //how to dismiss and delay animation for 2 secs!! like hinge?
+                                    self.isPresented.toggle()
+                                }
+                                
+                                
+                                
+                            }, label: {
+                                Image(systemName: "xmark.circle.fill")
+                                    .font(.system(size:50, weight: .semibold))
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(Color("red"))
+                                    .clipShape(Circle())
+                            })
+                            .padding(.trailing)
+                            .padding(.bottom, getSafeArea().bottom == 0 ? 12 : 0) //this is an if statement
+                            //.opacity(-scrollViewOffset > 450 ? 1 : 0)
+                            .animation(.easeInOut)
+                            
+                            //to show rejection transition
+                            .fullScreenCover(isPresented: $isPresented, content: {
+                                FullScreenModalView.init()
+                            })
+                            
+                            
+                            //fixing at bottom left the floating rejection !!
+                            , alignment: .bottomLeading
+                            
+                    )
+                    }
                 }
             }
             
@@ -183,6 +230,27 @@ struct MatchView: View {
         
     }
 }
+}
+
+struct FullScreenModalView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Image(systemName: "xmark.circle.fill")
+            .font(.system(size:50, weight: .semibold))
+            .foregroundColor(.white)
+            .padding()
+            .background(Color("red"))
+            .clipShape(Circle())
+    }
+}
+
+
+//extension view to get saferarea
+extension View{
+    func getSafeArea()->UIEdgeInsets {
+        return UIApplication.shared.windows.first?.safeAreaInsets ?? UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
 }
 
 struct MatchView_Previews: PreviewProvider {
